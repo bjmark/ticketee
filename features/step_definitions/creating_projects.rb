822f860ecd6b01f3a14c26bf8e0664371688ec0d
mark peng <bjmark@sina.com>
2011-12-18 10:09:58 +0800

    2011-12-18

--- features/step_definitions/creating_projects.rb
+++ features/step_definitions/creating_projects.rb
@@ -2,11 +2,11 @@ Given /^I am on the homepage$/ do
   visit('/')
 end
 
-When /^I follow "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /I follow "New Project"/ do 
+  click_link('New Project')
 end
 
-When /^I fill in "([^"]*)" with "([^"]*)"$/ do |arg1, arg2|
+When /I fill in "Name" with "TextMate 2"/ do 
   pending # express the regexp above with the code you wish you had
 end
 
